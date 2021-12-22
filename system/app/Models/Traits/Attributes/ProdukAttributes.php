ff2bb99040a9031e0e64292c1387a8b91a358dce
Muhammad Khadafi <90817924+MuhammadKhadafi07@users.noreply.github.com>
2021-12-22 13:30:38 -0800

    Tugas 10

---
+++ system/app/Models/Traits/Attributes/ProdukAttributes.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Models\Traits\Attributes;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+
+trait ProdukAttributes {
+
+    function getHargaStringAttribute(){
+        return "Rp. ".number_format($this->attributes['harga']);
+    }
+
+    function handleUploadfoto(){
+        $this->handleDelete();
+        if(request()->hasFile('foto')){
+            $foto = request()->file('foto');
+            $destination = "image/produk";
+            $RandomStr = Str::random(5);
+            $filename = $this->id."-".time()."-".$RandomStr.".".$foto->extension();
+            $url = $foto->storeAs($destination, $filename);
+            $this->foto = "app/".$url;
+            $this->save();
+        }
+    }
+
+    function handleDelete(){
+        $foto = $this->foto;
+        $path = public_path($foto);
+        if(file_exists($path)){
+            unlink($path);
+        }
+        return true;
+    }
+}
