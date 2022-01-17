b868065831294c44026508480c3d89c069e010fc
Muhammad Khadafi <90817924+MuhammadKhadafi07@users.noreply.github.com>
2022-01-16 23:52:42 -0800

    up

--- system/resources/views/produk/show.blade.php
+++ system/resources/views/produk/show.blade.php
@@ -10,20 +10,20 @@
                         Detail Data Produk
                     </div>
                     <div class="card-body">
-                        <h2>{{$produk->nama}}</h2>
+                        <h2>{{ $produk->nama }}</h2>
                         <hr>
-                            @include('produk.show.detail')
+                        @include('produk.show.detail')
                         <p>
                             {!! nl2br($produk->deskripsi) !!}
                         </p>
 
                         <p>
-                            <img src="{{url ("public",$produk->foto) }}" alt="">
+                            <img src="{{ url('public', $produk->foto) }}" alt="">
                         </p>
                     </div>
-                </div>  
+                </div>
             </div>
-        </div>   
+        </div>
     </div>
 
 @endsection
